e6a00b602fa10eb6e4feb4ebf1a20f9e832d5459
johnja <johnjairo.macias@hotmail.com>
2025-06-05 11:21:02 -0500

    initial: initial commit

---
+++ src/main/java/com/capitole/inditex/pricingms/PricingmsApplication.java
@@ -0,0 +1,13 @@
+package com.capitole.inditex.pricingms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricingmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricingmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capitole/inditex/pricingms/PricingmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capitole.inditex.pricingms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricingmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
